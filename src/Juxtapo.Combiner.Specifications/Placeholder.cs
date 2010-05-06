b856a33565b8fd29e8b28573676805c8fff9b44b
Arnold Zokas <arnold.zokas@coderoom.net>
2010-05-07 00:24:06 +0100

    Created initial solution structure

---
+++ src/Juxtapo.Combiner.Specifications/Placeholder.cs
@@ -0,0 +1,25 @@
+//  #######################################################
+//  
+//  # Copyright (C) 2010,  Dave Taylor and Arnold Zokas
+//  
+//  # This source code is subject to terms and conditions of the New BSD License.
+//  # A copy of the license can be found in the license.txt file at the root of this distribution.
+//  # If you cannot locate the New BSD License, please send an email to TODO or arnold.zokas@coderoom.net.
+//  # By using this source code in any fashion, you are agreeing to be bound by the terms of the New BSD License.
+//  # You must not remove this notice, or any other, from this software.
+//  
+//  #######################################################
+using Xunit;
+
+namespace Juxtapo.Combiner.Specifications
+{
+	public class Placeholder
+	{
+		[Fact]
+		public void JuxtapoCombinerIsReferenced()
+		{
+			// NOTE: This is added temporarily to ensure build script works
+			Assert.True(true);
+		}
+	}
+}
