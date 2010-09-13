19863077d6c040c7965de957fd82f84a975ca56d
Dave Tayor <dave@the-taylors.org>
2010-09-13 10:51:11 +0100

    update to example-combiner.js for use with cscript and rhino

--- example-combiner.js
+++ example-combiner.js
@@ -17,32 +17,89 @@
 ---------------------------------------------------*/
 (function() {
 
-    var combinerFileName = "juxtapo.js";
-    var includes = [];
-    includes.push("example1.js");
-    includes.push("example2.js");
+// UPDATE THE FOLLOWING
+var combinerFileName = "example-combiner.js";
+var includes = [];
+includes.push("jsfile.js");
+includes.push("folder/jsfile.js");
 
-	/* ********* DON'T CHANGE ANYTHING BELOW THIS LINE */
-    function getJsLocation(jsFileName) {
+	
+
+
+/* ********* DON'T CHANGE ANYTHING BELOW THIS LINE ******************************/
+    var dirSeparator;
+	function getJsLocation(jsFileName) {
+		var location;
 		if (typeof jsFileName === 'string') {
 			jsFileName = new RegExp(jsFileName.toLowerCase());
 		}
-		var scriptFiles = document.getElementsByTagName("script");
-		for (var i=0;i<scriptFiles.length;i+=1){
-			var scriptTag = scriptFiles[i];
-			var scriptFileName = scriptTag.src.substring(scriptTag.src.lastIndexOf("/")+1).toLowerCase();
-			if (jsFileName.test(scriptFileName)){
-				return scriptTag.src.substring(0,scriptTag.src.lastIndexOf("/")+1);
-			}
+		// If used within a browser
+		if (typeof document !== 'undefined' && typeof document.getElementsByTagName !== 'undefined'){
+			var scriptFiles = document.getElementsByTagName("script");
+			for (var i=0;i<scriptFiles.length;i+=1){
+				var scriptTag = scriptFiles[i];
+				var scriptFileName = scriptTag.src.substring(scriptTag.src.lastIndexOf("/")+1).toLowerCase();
+				if (jsFileName.test(scriptFileName)){
+					return scriptTag.src.substring(0,scriptTag.src.lastIndexOf("/")+1);
+				}
+			}			
+			throw 'Juxtapo Combiner could not match ' + jsFileName + ' to any embeded script references';
+		} 
+		// is used with rhino
+		else if (typeof java !== 'undefined'){
+			location = new java.io.File(environment['user.dir']).toString();
+		} 
+		// if used with cscript
+		else if (WScript){
+			location = WScript.CreateObject("WScript.Shell").CurrentDirectory;
+		}else{
+			throw 'Juxtapo Combiner could not retrieve the working directory';			
 		}
-		throw 'Juxtapo Combiner could not match ' + jsFileName + ' to any embeded script references';
-		return null;
+		dirSeparator = location.indexOf('\\') === -1 ? '/' : '\\';
+		location = location.substr(location.length-1,1) === dirSeparator ? location : location+dirSeparator;
+		return location;
     };
     var combinerJsLocation = getJsLocation(combinerFileName);
+	function resolveAbsoluteUrl(baseUrl,relativeUrl) {
+		try {
+			if (relativeUrl.substr(0, 1) === '/') {
+				return baseUrl.substring(0, baseUrl.indexOf('/', baseUrl.indexOf('//') + 2)) + relativeUrl;
+			}
+			else {
+				relativeUrl = dirSeparator === '/' ? relativeUrl : relativeUrl.replace(/\//g,'\\');
+				var Loc = baseUrl;
+				Loc = Loc.substring(0, Loc.lastIndexOf(dirSeparator));
+				while (/^\.\./.test(relativeUrl)) {
+					Loc = Loc.substring(0, Loc.lastIndexOf(dirSeparator));
+					relativeUrl = relativeUrl.substring(3);
+				}
+				return Loc + dirSeparator + relativeUrl;
+			}
+		} 
+		catch (ex) {
+			print('d:'+ex);
+		}
+	}
     function includeJs() {
-        for (var i = 0; i < includes.length; i++) {
-            document.write("<script type=\"text/javascript\" src=\"" + combinerJsLocation + includes[i] + "\"></script>");
-        }
+		for (var i = 0; i < includes.length; i++) {
+			var src = resolveAbsoluteUrl(combinerJsLocation, includes[i]);
+
+			// If used within a browser
+			if (typeof document !== 'undefined' && typeof document.getElementsByTagName !== 'undefined') {
+				document.write("<script type=\"text/javascript\" src=\"" + src + "\"></script>");
+			}
+			// is used with rhino
+			else if (typeof load !== 'undefined') {
+				load(src);
+			}
+			// if used with cscript
+			else if (typeof ActiveXObject !== 'undefined') {
+				eval(new ActiveXObject("Scripting.FileSystemObject").OpenTextFile(src, 1).ReadAll());
+			}
+			else {
+				throw ('combiner is not compatible with this JavaScript engine');
+			}
+		}
         return null;
     };
     includeJs();
