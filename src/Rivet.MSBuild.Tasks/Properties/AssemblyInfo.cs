847ab8b1209f43f644a6b75daf58b38f35bc52d3
ArnoldZokas <arnold.zokas@coderoom.net>
2010-09-25 01:25:50 +0100

    Created projects for Rivet MSBuild tasks

---
+++ src/Rivet.MSBuild.Tasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// #######################################################
+// 
+// # Copyright (C) 2010, Dave Taylor and Arnold Zokas
+// 
+// # This source code is subject to terms and conditions of the New BSD License.
+// # A copy of the license can be found in the license.txt file at the root of this distribution.
+// # If you cannot locate the New BSD License, please send an email to dave@the-taylors.org or arnold.zokas@coderoom.net.
+// # By using this source code in any fashion, you are agreeing to be bound by the terms of the New BSD License.
+// # You must not remove this notice, or any other, from this software.
+// 
+// #######################################################
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rivet MSBuild Tasks")]
+[assembly: AssemblyDescription("MSBuild tasks for combining javascript files")]
+[assembly: AssemblyCopyright("Copyright © 2010, Dave Taylor and Arnold Zokas")]
+[assembly: Guid("b032aa5d-6e97-4518-a6a9-23a850de590a")]
+[assembly: ComVisible(false)]
